afb8a0cc78ff8a42b439d57c859fa79be5a43854
DimaSimakov <dimarodeo@gmail.com>
2023-12-27 20:14:19 +0700

    Add list_filter and supplier_url in companies.admin.py

--- companies/admin.py
+++ companies/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
-from companies.models import Company
+from django.utils.html import format_html
 
-# admin.site.register(Company)
+from companies.models import Company
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('company_name', 'company_city', 'company_type', 'company_supplier', 'debt_to_supplier')
+    list_display = ('company_name', 'company_city', 'company_type', 'supplier_url',
+                    'debt_to_supplier')
+    list_filter = ('company_city',)
     actions = ['nullify_debt']
 
     def nullify_debt(self, request, queryset):
@@ -17,3 +19,16 @@ class CompanyAdmin(admin.ModelAdmin):
         self.message_user(request, f'Задолженность перед поставщиком у выбранных покупателей обнулена.')
 
     nullify_debt.short_description = 'Обнулить задолженность'
+
+    def supplier_url(self, obj):
+        """ Получение ссылки на поставщика """
+
+        supplier = obj.company_supplier  # получаем поставщика
+        try:
+            supplier_id = supplier.id
+            url = f'{supplier_id}/change/'  # формируем адрес
+            return format_html("<a href='{url}'>{name}</a>", url=url, name=obj.company_supplier)
+        except AttributeError:
+            pass
+
+    supplier_url.short_description = 'Поставщик'
